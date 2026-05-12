9908e6fa9906f4a30c33b7f84c60ec4f77c11365
ArthurDepuydt04 <arthur@branderij.eu>
2026-05-12 10:39:04 +0200

    klaar met navbar

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -1,21 +1,41 @@
 import { useState } from "react";
+import searchIcon from "../../assets/search.svg";
 import "./Input.css";
 
 function Input({ value, setValue, type, id, name, label, style, placeholder }) {
   if (label === undefined) {
-    return (
-      <>
-        <input
-          type={type}
-          id={id}
-          name={name}
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-          className={style}
-          placeholder={placeholder}
-        />
-      </>
-    );
+    if (id === "search") {
+      return (
+        <div className="input-wrapper">
+          <>
+            <input
+              type={type}
+              id={id}
+              name={name}
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+              className={style}
+              placeholder={placeholder}
+            />
+            <img src={searchIcon} alt="Search" className="search-icon " />
+          </>
+        </div>
+      );
+    } else {
+      return (
+        <>
+          <input
+            type={type}
+            id={id}
+            name={name}
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            className={style}
+            placeholder={placeholder}
+          />
+        </>
+      );
+    }
   } else
     return (
       <>
